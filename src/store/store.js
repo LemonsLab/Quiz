35d824fd40cceb2fbd5fbc53789f79f0bd405212
PiotrA <piotrandrzejewski@MacBook-Pro-Piotr.local>
2017-09-03 22:12:19 +0200

    fix store configuration

--- src/store/store.js
+++ src/store/store.js
@@ -1,19 +1,28 @@
 /**
  * Created by piotrandrzejewski on 03.09.2017.
  */
-import { createStore } from 'redux';
+import {
+    createStore,
+    combineReducers
+} from 'redux';
 import  reducerSettings from '../redux/reducer/index'
 
 
+const reducer = combineReducers({
+    reducerSettings:reducerSettings,
 
-const store = createStore({
-
-    SettingsReducer: reducerSettings
 });
 
 
 
 
+
+
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+);
 
 
 
