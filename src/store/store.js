1d28a73e79914ee5a356591cc148a6349bbfdb4d
PiotrA <piotrandrzejewski@MacBook-Pro-Piotr.local>
2017-09-03 19:40:48 +0200

    simple implementation of actions in settings reducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,3 +1,20 @@
 /**
  * Created by piotrandrzejewski on 03.09.2017.
  */
+import { createStore } from 'redux';
+import  reducerSettings from '../redux/reducer/index'
+
+
+
+const store = createStore({
+
+    SettingsReducer: reducerSettings
+});
+
+
+
+
+
+
+
+export default store;
