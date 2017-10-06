b86096a8af695ebce75b9440275a6606dbd2b585
Adam Wejler <reljewa@op.pl>
2017-10-06 18:59:15 +0200

    elo ziomeczku

--- src/AppView.js
+++ src/AppView.js
@@ -4,7 +4,7 @@ import Test from './components/Test/Test';
 import Settings         from './components/Settings/Settings.js';
 import YourTests        from './components/YourTests/YourTests.js';
 import YourTestViewContainer     from './containers/YourTestViewContainer/YourTestViewContainer.js';
-import Menu from './components/Menu/Menu'
+import MenuCard from './containers/MenuContainer/Menu'
 import ResultsWindow    from './components/ResultsWindow/ResultsWindow.js';
 
 
@@ -12,7 +12,7 @@ class AppView extends React.Component {
 
     render(){
         return(
-          <ResultsWindow/>
+          <MenuCard/>
         )
     }
 }
