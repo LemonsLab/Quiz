6df26fe187856eed30cdcb623ec994de1e309643
PiotrA <piotrandrzejewski@MacBook-Pro-Piotr.local>
2017-10-21 18:39:12 +0200

    connect app with provider

--- index.android.js
+++ index.android.js
@@ -1,17 +1,17 @@
 import AppView from './src/AppView';
 import React, {Component} from 'react';
 import {AppRegistry} from 'react-native';
-import {NavigationActions} from 'react-navigation';
-
+import store from './src/store/store'
+import {Provider} from 'react-redux'
 class PepperoniAppTemplate extends Component {
 
 
 
   render() {
     return (
-
+      <Provider store={store}>
         <AppView />
-
+      </Provider>
     );
   }
 }
